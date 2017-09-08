2fb2199ff9ab474be3eaa873202d2121b20f3585
Dominik Herold <d.herold@cid.biz>
2017-09-08 12:31:00 +0200

    use ngOnInit

--- ClientApp/app/components/eventdata/eventdata.component.ts
+++ ClientApp/app/components/eventdata/eventdata.component.ts
@@ -1,21 +1,23 @@
-﻿import { Component, Inject } from '@angular/core';
+﻿import { Component, Inject, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
 
 @Component({
     selector: 'eventdata',
     templateUrl: './eventdata.component.html'
 })
-export class EventDataComponent {
+export class EventDataComponent implements OnInit {
+    ngOnInit(): void {
+        this.http.get(this.originUrl + '/api/EventData/GetEventData').subscribe(result => {
+            this.events = result.json() as IEventData[];
+        }); }
+
     public events: any;
 
-    constructor(http: Http, @Inject('ORIGIN_URL') originUrl: string) {
-        http.get(originUrl + '/api/EventData/GetEventData').subscribe(result => {
-            this.events = result.json() as EventData[];
-        });
+    constructor(private http: Http, @Inject('ORIGIN_URL') private originUrl: string) {
     }
 }
 
-interface EventData {
+interface IEventData {
     id: any;
     name: any;
     date: any;
