d1755a137cea49d112915d9c691059ae4eaa5381
Dominik Herold <d.herold@cid.biz>
2017-09-08 12:09:41 +0200

    load events from API

---
+++ Controllers/EventDataController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace AngularTest.Controllers
+{
+    [Route("api/[controller]")]
+    public class EventDataController : Controller
+    {
+        public class EventData
+        {
+            public readonly string Id;
+
+            public readonly string Name;
+
+            public readonly string Date;
+
+            public readonly string Time;
+
+            public EventData(string id, string name, string date, string time)
+            {
+                Id = id;
+                Name = name;
+                Date = date;
+                Time = time;
+            }
+        }
+
+        [HttpGet("[action]")]
+        public IEnumerable<EventData> GetEventData()
+        {
+            yield return new EventData("1", "FirstName", "FirstDate", "FirstTime");
+            yield return new EventData("2", "SecondName", "SecondDate", "SecondTime");
+        }
+    }
+}
